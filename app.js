2ffc7768021337dd61d69fbebc9ca28da5c4b034
EuroDrop <catarinaf928@gmail.com>
2026-08-03 23:57:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import { getDatabase, onValue, ref, set } from 'https://www.gstatic.com/firebase
   const DEVICE_ID_KEY = 'massageCreditsDeviceId_v1';
   const FIREBASE_STATE_PATH = 'apps/massageCredits/sharedState';
   const MAX_HISTORY = 1000;
-  const DEFAULT_PIN = '1234';
+  const DEFAULT_PIN = 'const';
 
   const firebaseConfig = {
     apiKey: 'AIzaSyBTGZU1zoxw3Utacst8gU3ZIiY9uJ-rnuA',
